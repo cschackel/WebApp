f72ba2adcceeccd475f842ff0d58545bc1afeb45
Cyrus Schackel <cschackel@my.wctc.edu>
2020-01-20 20:05:45 -0600

    Added Data Models and JS/CSS Files

---
+++ Modas/Models/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Modas.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool Flagged { get; set; }
+        // foreign key for location 
+        public int LocationId { get; set; }
+        // navigation property
+        public Location Location { get; set; }
+
+        public String toString()
+        {
+            String output = "{\n";
+            output += $"Event ID: {EventId}\n";
+            output += $"DateTime: {TimeStamp}\n";
+            output += $"Flagged: {Flagged}\n";
+            output += $"Location: {Location}\n";
+            output += "}";
+            return output;
+        }
+    }
+}
---
+++ Modas/Models/Location.cs
@@ -0,0 +1,17 @@
+﻿namespace Modas.Models
+{
+    public class Location
+    {
+        public int LocationId { get; set; }
+        public string Name { get; set; }
+
+        public string toString()
+        {
+            string output = "{\n";
+            output += $"Location ID: {LocationId}\n";
+            output += $"Name: {Name}\n";
+            output += "}";
+            return output;
+        }
+    }
+}
